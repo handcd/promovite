33b81934693f8346c11d52087bcf65e506d9c231
pablocontreras <pablo_contreras_1997@outlook.com>
2018-08-21 16:05:20 -0500

    Problema de vista principal arreglada

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,13 @@ Route::get('/', function () {
 Route::get('/index', function () {
     return view('inicio');
 });
-Route::get('/inicio', function() {
+/*Route::get('/inicio', function() {
     return view('inicio');
-});
+});*/
 Auth::routes();
 //Todas las rutas de las categorias:(
+Route::get('/','categoriasController@indexForEscrituraWinideas');
+
 Route::get('/escritura','categoriasController@indexForEscrituraWinideas');
 Route::get('/bolsas-maletas-textiles','categoriasController@indexForBolsasWinideas');
 Route::get('/hogar','categoriasController@indexForHogarWinideas');
