dc624fefa0da17b3d8ee1610fea1066d35b9205b
pablocontreras <pablo_contreras_1997@outlook.com>
2019-02-21 15:14:47 -0600

    Cambio de precio innova

--- resources/views/categories/hogar.blade.php
+++ resources/views/categories/hogar.blade.php
@@ -47,6 +47,16 @@
                     <li class="list-group-item">Consultar el precio con su agente de ventas</li>
                   </ul>
                   @break
+                @case('PIN')
+                    <h4 class="card-header text-white winideas">Modelo <br>{{ $informacion->catalogo }}-{{ $informacion->modelo }}</h4>  
+                    <img src="{{ asset('img/'.$informacion->categoria.'/'.$informacion->catalogo.'/'.$informacion->modelo.'/'.$informacion->modelo.'_'.$informacion->color.'.jpg') }}" alt="Imagen no disponible" style="display: block;  max-height: 300px">
+                    <div class="card-body">
+                      <p class="card-text"> {{strtoupper($informacion->descripcion) }}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item">Precio unitario: ${{ round($informacion->precio_distribuidor, 2) }}</li>
+                    </ul>
+                    @break
                 @case('PMD')
                   <h4 class="card-header text-white winideas">Modelo <br>{{ $informacion->catalogo }}-{{ $informacion->modelo }}</h4>  
                   <img src="{{ asset('img/'.$informacion->categoria.'/'.$informacion->catalogo.'/'.$informacion->modelo.'/'.$informacion->modelo.'-'.$informacion->color.'.jpg') }}" alt="Imagen no disponible" style="display: block;  max-height: 300px">
